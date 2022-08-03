85559f3e965d0e154e4700f94712434c6b171c5f
edwardsuwirya <edward_suwirya@yahoo.com>
2022-08-03 12:20:44 +0700

    [IMPROVE] Change to full redux

--- src/features/studentBook/StudentBookView.js
+++ src/features/studentBook/StudentBookView.js
@@ -4,17 +4,19 @@ import {LABEL} from "../../app/constants";
 import withContainer from "../../components/withContainer";
 
 const StudentBookView = (props) => {
-    const {handleAddBook, handleSubmit, student, newBookValue, handleNewBookChange, books} = props;
+    const {handleSubmit, student, newBookValue, handleNewBookChange, books} = props;
     return (
         <>
-            <div>{student.name}</div>
-            <AppFormInput id='book' label={LABEL["Book.label"].value} value={newBookValue}
-                          onValueChange={handleNewBookChange}/>
-            <AppButton handleClick={handleAddBook} label={LABEL["AddBook.label"].value}/>
-            <AppButton handleClick={handleSubmit} label={LABEL["StudentBook.submit"].value}/>
-            <ul>
-                {books.map(book => <li key={book}>{book}</li>)}
-            </ul>
+            <div style={{margin: '30px'}}>
+                <h2>Book</h2>
+                <div>{student.name}</div>
+                <AppFormInput id='book' label={LABEL["Book.label"].value} value={newBookValue}
+                              onValueChange={handleNewBookChange}/>
+                <AppButton handleClick={handleSubmit} label={LABEL["StudentBook.submit"].value}/>
+                <ul>
+                    {books.map(book => <li key={book}>{book}</li>)}
+                </ul>
+            </div>
         </>
     )
 }
